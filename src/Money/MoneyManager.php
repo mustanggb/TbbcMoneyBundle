4d95abd34b7c90d8d9e7a118b713764d87d0602e
MustangGB <mustang@miswave.com>
2024-02-25 14:45:05 +0000

    Fix psalm

--- src/Money/MoneyManager.php
+++ src/Money/MoneyManager.php
@@ -21,7 +21,7 @@ class MoneyManager implements MoneyManagerInterface
      */
     public function createMoneyFromFloat(float $floatAmount, ?string $currencyCode = null): Money
     {
-        if (is_null($currencyCode)) {
+        if (is_null($currencyCode) || '' === $currencyCode) {
             $currencyCode = $this->referenceCurrencyCode;
         }
         $currency = new Currency($currencyCode);
