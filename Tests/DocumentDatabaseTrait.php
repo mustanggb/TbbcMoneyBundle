e9ee4472de2e96d8d3229e230b6ec37076a850e9
MustangGB <mustang@miswave.com>
2024-08-27 15:00:44 +0100

    Update code

--- Tests/DocumentDatabaseTrait.php
+++ Tests/DocumentDatabaseTrait.php
@@ -49,8 +49,8 @@ trait DocumentDatabaseTrait
         $code = $application->run(new ArrayInput([
             'command' => 'doctrine:mongodb:schema:drop',
         ]), new BufferedOutput());
-        print_r($wasd->fetch(), true);
-        self::assertSame('', $wasd->fetch());
+        fwrite(STDERR, print_r($wasd, true));
+        self::assertSame('', $wasd);
         self::assertSame(Command::SUCCESS, $code);
     }
 }
